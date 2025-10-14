a6455ea76c0d87ca66802372ecce3899e7049ea4
zbtzbt44 <76580908+zbtzbt44@users.noreply.github.com>
2025-10-14 16:42:49 +0800

    Update deferredgs.sh

--- scripts/deferredgs.sh
+++ scripts/deferredgs.sh
@@ -1,6 +1,6 @@
 device_id=1
 
-dataset_path=data/refnerf/coffee
+dataset_path=data/dataset/refnerf/coffee
 output_path=output/coffee
 
 CUDA_VISIBLE_DEVICES=${device_id} python 2dgs_trainer.py -s ${dataset_path} -m ${output_path}
@@ -8,5 +8,5 @@ CUDA_VISIBLE_DEVICES=${device_id} python 2dgs_trainer.py -s ${dataset_path} -m $
 CUDA_VISIBLE_DEVICES=${device_id} python render_2dgs.py -s ${dataset_path} -m ${output_path}
 
 #relighting
-novel_brdf_envmap=/mnt/155_16T/zhangbotao/jgaussian/data/env/resized_32x16/city.exr
+novel_brdf_envmap=data/env/city.exr
 CUDA_VISIBLE_DEVICES=${device_id} python render_2dgs.py -s ${dataset_path} -m ${output_path} --novel_brdf_envmap ${novel_brdf_envmap}
