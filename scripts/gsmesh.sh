5bc425b0d641d5d5a7ab79380107575fb9c61479
zbtzbt44 <76580908+zbtzbt44@users.noreply.github.com>
2025-10-14 16:45:34 +0800

    Update gsmesh.sh

--- scripts/gsmesh.sh
+++ scripts/gsmesh.sh
@@ -1,7 +1,7 @@
 device_id=1
 
-dataset_path=/mnt/155_16T/zhangbotao/jgaussian/nerf_synthetic/lego
-output_path=/mnt/155_16T/zhangbotao/jgaussian/output/lego
+dataset_path=data/dataset/lego
+output_path=output/lego-gsmeesh
 
 input_mesh=/mnt/155_16T/zhangbotao/jgaussian/data/MESH/lego/1/1.obj
 
