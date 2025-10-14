ef3ec396ef626e2a8059738f83bceffdb59347cf
zbtzbt44 <76580908+zbtzbt44@users.noreply.github.com>
2025-10-14 16:31:47 +0800

    Update gsheadrelighting.sh

--- scripts/gsheadrelighting.sh
+++ scripts/gsheadrelighting.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 device_id=1
-network_pkl=/mnt/155_16T/zhangbotao/jgaussian/checkpoints/gshead.pkl
+network_pkl=data/ckpt/gshead.pkl
 resolution=512
 
 trunc=0.7
 seeds=0,9
 opacity_ones=False
-# lighting_pattern='transfer'
-# lighting_pattern='circle'
 lighting_pattern='envmap'
 outdir=out
 grid=1x1
-# lighting_transfer_ids=2,12,30,34
-# lighting_transfer_ids=38,62,68,82
 lighting_transfer_ids=62,68,82,90
 with_bg=False
 
