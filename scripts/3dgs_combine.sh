ee77ff0e445c51a208514540e10ffd4cbc33b36e
zbtzbt44 <76580908+zbtzbt44@users.noreply.github.com>
2025-10-14 16:40:27 +0800

    Update 3dgs_combine.sh

--- scripts/3dgs_combine.sh
+++ scripts/3dgs_combine.sh
@@ -1,9 +1,9 @@
 device_id=1
 
-dataset_path=/mnt/155_16T/zhangbotao/jgaussian/nerf_synthetic/chair
-output_path=/mnt/155_16T/zhangbotao/jgaussian/output/chair
+dataset_path=data/dataset/lego
+output_path=output/chair
 
-input_mesh=/mnt/155_16T/zhangbotao/jgaussian/data/MESH/chair/1.obj
+input_mesh=data/dataset/lego/mesh/1.obj
 
 #gaussian_mesh training(with apply_weights in seg-gaussian)
 CUDA_VISIBLE_DEVICES=${device_id} python 3dgs_trainer.py -s ${dataset_path} -m ${output_path} --input_mesh ${input_mesh}
@@ -12,12 +12,12 @@ CUDA_VISIBLE_DEVICES=${device_id} python 3dgs_trainer.py -s ${dataset_path} -m $
 iteration=15000
 gaussian_name=point_cloud/iteration_${iteration}/point_cloud.ply
 pre_gaussian_path=${output_path}/${gaussian_name}
-style_image="/mnt/155_16T/zhangbotao/jgaussian/data/style/3.jpg"
+style_image="data/style/starry_night.jpg"
 CUDA_VISIBLE_DEVICES=${device_id} python 3dgs_trainer.py -s ${dataset_path} -m ${output_path} --point_cloud ${pre_gaussian_path} \
     --style ${style_image}  --is_stylized --histgram_match --input_mesh ${input_mesh}
 
 
-deform_mesh=/mnt/155_16T/zhangbotao/jgaussian/data/MESH/chair/2.obj
+deform_mesh=data/dataset/lego/mesh/2.obj
 #gaussian_mesh rendering
 CUDA_VISIBLE_DEVICES=${device_id} python render.py -s ${dataset_path} -m ${output_path}
 #gaussian_mesh deformation rendering
