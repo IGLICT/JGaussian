54d247d20962e112eee71e875d099889955a6a64
zbtzbt44 <76580908+zbtzbt44@users.noreply.github.com>
2025-10-14 16:48:23 +0800

    Update gsmesh.sh

--- scripts/gsmesh.sh
+++ scripts/gsmesh.sh
@@ -1,14 +1,14 @@
 device_id=1
 
 dataset_path=data/dataset/lego
-output_path=output/lego-gsmeesh
+output_path=output/lego-gsmesh
 
-input_mesh=/mnt/155_16T/zhangbotao/jgaussian/data/MESH/lego/1/1.obj
+input_mesh=data/dataset/lego/mesh/1.obj
 
 CUDA_VISIBLE_DEVICES=${device_id} python 3dgs_trainer.py -s ${dataset_path} -m ${output_path} --input_mesh ${input_mesh}
 
 
-deform_mesh=/mnt/155_16T/zhangbotao/jgaussian/data/MESH/lego/1/1.obj
+deform_mesh=data/dataset/lego/mesh/2.obj
 
 CUDA_VISIBLE_DEVICES=${device_id} python render.py -s ${dataset_path} -m ${output_path}
 #render deformed gaussian
