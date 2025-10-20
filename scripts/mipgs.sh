7edf04d58892b38a10003a32cef04d3752d439b1
zbtzbt44 <76580908+zbtzbt44@users.noreply.github.com>
2025-10-20 17:14:33 +0800

    Update mipgs.sh

--- scripts/mipgs.sh
+++ scripts/mipgs.sh
@@ -1,7 +1,7 @@
-device_id=2
+device_id=0
 
-dataset_path=/mnt/155_16T/zhangbotao/jgaussian/nerf_synthetic/lego
-output_path=/mnt/155_16T/zhangbotao/jgaussian/output/legomip
+dataset_path=data/dataset/lego
+output_path=output/lego-mip
 
 CUDA_VISIBLE_DEVICES=${device_id} python mip_trainer.py -s ${dataset_path} -m ${output_path}
 CUDA_VISIBLE_DEVICES=${device_id} python mip_render.py -s ${dataset_path} -m ${output_path}
